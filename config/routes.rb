402a01b71030608bdbb211a53fbd6359faa1dc0d
David French <marsstrum@gmail.com>
2014-12-09 01:03:38 +0000

    Added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Softmarks::Application.routes.draw do
   devise_for :users
+  resources :users, only: [:update]
   get "welcome/index"
   get "welcome/about"
  
-  #root to: 'welcome#index'
-  root to: 'devise/registrations#new'
+  root to: 'welcome#index'
+  #root to: 'devise/registrations#new'
 end
