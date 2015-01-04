edde8c4b390886fba76355617a5b3e3ad0e1696f
David French <marsstrum@gmail.com>
2015-01-04 18:13:24 +0000

    Incoming controller changes for testing

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,14 +6,32 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
-    # sender: "dave@gmaiol.com", subject: "my subject"....
+    #puts "INCOMING PARAMS HERE: #{params}"
+    puts " ***** USER EMAIL: #{params[:sender]}"
+    puts " ***** TOPIC: #{params[:subject]}"
+    puts " ***** BOOKMARK: #{params['stripped-text']}"
+      # sender: "dave@gmaiol.com", subject: "my subject"....
+      # topic = regex on subject here to get topic name (steelers instead of #steelers)
+      # subject: #steelers #bengals #nfl
+      # http://highlightofgame.com
 
-    # @topic = Topic.find_or_create_by_name(params[:topic][:name])
-    # @bookmark = Bookmark.find_or_create_by_url(url: url)
-    # @user = User.find_by_email(params[:sender])
+      # @bookmark = Bookmark.find_or_create_by_url(params['stripped-text'])
+    # @user = User.find_by_email(params[:sender]) 
+
+      # @topics = params[:subject].gsub(/(^#)|(\s#)/,'').split(' ')
+      # .. @topics = ["steelers", "bengals", "nfl"]
+      # to get there from above, split on spaces, then do a regexx replace of # (and replace it with '')
+      # iterate through topics and find or create by name for each one
+      # @topics.each do |topic_name|
+          # topic = Topic.find_or_create_by_name(topic_name)
+          # add bookmark to topic unless topic includes bookmark
+      # end
+      # add bookmark to user unless user include bookmark
 
     
+    
+
+    ^#
     # You put the message-splitting and business
     # magic here. 
 
