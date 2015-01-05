7a1dabee2d021e56d44a1ac172494938b5c0bab9
David French <marsstrum@gmail.com>
2015-01-05 00:54:03 +0000

    fix regex

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -18,7 +18,8 @@ class IncomingController < ApplicationController
       @bookmark = Bookmark.find_or_create_by_url(params['stripped-text'])
       @user = User.find_by_email(params[:sender]) 
 
-      @topics = params[:subject].gsub(/(^#+)|(\s#)/,'').split(' ')
+      #@topics = params[:subject].gsub(/(^#+)|(\s#)/,'').split(' ')
+      @topics = params[:subject].gsub(/(^#+)/,'').split(' ')
       # .. @topics = ["steelers", "bengals", "nfl"]
       # to get there from above, split on spaces, then do a regexx replace of # (and replace it with '')
       # iterate through topics and find or create by name for each one
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,6 @@ class Bookmark < ActiveRecord::Base
   belongs_to :topic
   has_many :like_bookmarks
   has_many :users, through: :like_bookmarks
+
+  default_scope { order('updated_at DESC')}
 end
