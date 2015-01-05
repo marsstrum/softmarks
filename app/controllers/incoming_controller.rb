7c38691a41ed47b8d78c1680c92dd133cb2f5617
David French <marsstrum@gmail.com>
2015-01-05 02:11:47 +0000

    revised controller with gsub

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -19,11 +19,12 @@ class IncomingController < ApplicationController
       @user = User.find_by_email(params[:sender]) 
 
       #@topics = params[:subject].gsub(/(^#+)|(\s#)/,'').split(' ')
-      @topics = params[:subject].split(' ').gsub(/(^#+)/,'')
+      @topics = params[:subject].split(' ')
       # .. @topics = ["steelers", "bengals", "nfl"]
       # to get there from above, split on spaces, then do a regexx replace of # (and replace it with '')
       # iterate through topics and find or create by name for each one
       @topics.each do |topic_name|
+          topic_name.gsub!(/^#/,'')
           topic = Topic.find_or_create_by_name(topic_name)
             if !@bookmark.topic 
               @bookmark.topic = topic
