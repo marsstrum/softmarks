9cb024bd487ee650449e087e9de76abcd6a5b1f8
David French <marsstrum@gmail.com>
2015-01-15 02:56:39 +0000

    url images using Embedly

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,4 +6,21 @@ class Bookmark < ActiveRecord::Base
   has_many :users, through: :like_bookmarks
 
   default_scope { order('updated_at DESC')}
+
+  after_create :set_embedly_url
+
+	def set_embedly_url
+    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY'],
+          :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
+ 
+    obj = embedly_api.extract :url => self.url
+    # get result and find the attribute for the image
+    # set the bookmark's image with that value -- update_attribute(image: val_of_image_from_response)
+    o = obj.first
+    image = o.images.first
+
+    update_attribute(:image, image['url']) if image && image['url'].present?
+    true
+  end
+
 end
